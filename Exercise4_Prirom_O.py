3cf690cfa4d8c6d1713e957d65fd99a5ec3bd405
ihunz1122 <95174362+ihunz1122@users.noreply.github.com>
2021-11-28 23:17:59 +0700

    แก้ไขเพิ่มชื่อและคะแนน

--- Exercise4_Prirom_O.py
+++ Exercise4_Prirom_O.py
@@ -3,7 +3,29 @@ age = 32
 height = 175
 weigth = 65
 
-print(name)
-print(age)
-print(height)
-print(weigth)
+print("ชื่อ  : ", name)
+print("อายุ : ", age)
+print("ส่วนสูง : ", height)
+print("น้ำหนัก : ", weigth)
+
+print("                                    ")
+
+"""
+Foundation English = Foundation_English
+General Business = General_Business
+Introduction to Computer Systems = Introduction_to_Computer_Systems
+Computer Programming = Computer_Programming
+"""
+
+# ตัวแปร
+Foundation_English = FE = 10
+General_Business = GB = 20
+Introduction_to_Computer_Systems = ICS = 30
+Computer_Programming = CP = 40
+
+print("--- Your Score ---")
+
+print("Foundation English : ", "                ", FE)
+print("General Business : ", "                  ", GB)
+print("Introduction to Computer Systems : ", "  ", ICS)
+print("Computer Programming : ", "              ", CP)
