eef8d6fe1f5de02b17870272d80262bf5c71ff33
ihunz1122 <95174362+ihunz1122@users.noreply.github.com>
2021-12-03 17:11:24 +0700

    เพิ่มการบ้าน

---
+++ Lecture 50  การสร้าง  ใช้งานฟังก์ชันในภาษา Python 3.py
@@ -0,0 +1,16 @@
+def addNumber1(x,y):
+    print(x+y)
+addNumber1(10,5)
+
+def addNumber2(x,y):
+    print(x-y)
+addNumber2(10,5)
+
+def addNumber3(x,y):
+    print(x*y)
+addNumber3(10,5)
+
+def addNumber4(x,y):
+    print(x/y)
+addNumber4(10,5)
+
