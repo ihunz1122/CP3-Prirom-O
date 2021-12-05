9b7b9b5417c0fd32993412829300a30447027b7b
ihunz1122 <95174362+ihunz1122@users.noreply.github.com>
2021-12-05 15:21:48 +0700

    เพิ่มการบ้าน

---
+++ Lecture_54_Prirom_O.py
@@ -0,0 +1,35 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        return False
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+    return menuSelect()
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1:
+        price = int(input("ราคาสินค้า: "))
+        print(vatCalculator(price))
+        return showMenu()
+    elif userSelected == 2:
+        print(priceCalculator())
+        return showMenu()
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+login()
